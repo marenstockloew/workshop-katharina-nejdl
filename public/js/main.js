16158157e5172e5096eab9446a8e1017170ef098
marenstockloew <m.stockloew@posteo.de>
2023-05-25 11:32:30 +0200

    Update

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,12 @@ document.body.style.backgroundColor =
 document.body.style.color =
     "rgba( " + (1/widthOutput) * 255 * 800 + ",0," + (widthOutput/1500) * 255 + ", 1)";
 
+document.getElementById("textinput").style.color = 
+    "rgba( " + (1/widthOutput) * 255 * 800 + ",0," + (widthOutput/1500) * 255 + ", 1)";
+
+document.getElementById("submitbutton").style.color = 
+    "rgba( " + (1/widthOutput) * 255 * 800 + ",0," + (widthOutput/1500) * 255 + ", 1)";
+
 var resizeColor = function(e) {
     //note i need to pass the event as an argument to the function
     widthOutput = e.target.innerWidth;
@@ -15,21 +21,25 @@ var resizeColor = function(e) {
         "rgba( 0, " + (1/widthOutput) * 255 * 800 + "," + (widthOutput/1500) * 255 + ", 1)";
     document.body.style.color =
         "rgba( " + (1/widthOutput) * 255 * 800 + ",0," + (widthOutput/1500) * 255 + ", 1)";
+    document.getElementById("textinput").style.color = 
+        "rgba( " + (1/widthOutput) * 255 * 800 + ",0," + (widthOutput/1500) * 255 + ", 1)";
+    document.getElementById("submitbutton").style.color = 
+        "rgba( " + (1/widthOutput) * 255 * 800 + ",0," + (widthOutput/1500) * 255 + ", 1)";
  }
 
 // BORDER RADII
 
 // document.getElementsByClassName("grid__element").style.borderRadius = 
-document.getElementById("text-container1").style.borderRadius = 
-    (1/widthOutput) * 10000 + "px";
+// document.getElementById("text-container1").style.borderRadius = 
+//     (1/widthOutput) * 10000 + "px";
 
-var resizeRadius = function(e) {
-    widthOutput = e.target.innerWidth;
-    document.getElementsByClassName("grid__element").style.borderRadius = 
-        (1/widthOutput) * 10000 + "px";
- }
+// var resizeRadius = function(e) {
+//     widthOutput = e.target.innerWidth;
+//     document.getElementsByClassName("grid__element").style.borderRadius = 
+//         (1/widthOutput) * 10000 + "px";
+//  }
 
 // ALL EVENT LISTENERS
 
 window.addEventListener("resize", resizeColor);
-window.addEventListener("resize", resizeRadius);
+// window.addEventListener("resize", resizeRadius);
